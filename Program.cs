3864f3f7c7648207b4d5f17fbb406c9c05198c7d
vpganapathy2@gmail.com <vpganapathy2@gmail.com>
2021-07-11 23:22:39 -0700

    calculator

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿  using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            add(2, 5);
+           
+
+        }
+
+
+
+        public static int add( int x,int y)
+        {
+
+
+            int z = x + y;
+            return z;
+        }
+    }
+
+
+
+}
